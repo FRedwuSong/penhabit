4cf811b2c80dcb621dcee364333e86d55ed46bc3
wusunghan   <diadora1166@gmail.com>
2018-05-28 23:12:58 +0800

    pen add edit and action

--- app/controllers/pens_controller.rb
+++ app/controllers/pens_controller.rb
@@ -17,6 +17,20 @@ before_action :authenticate_user!
 		end
 	end
 
+	def edit
+		 @pen = Pen.find_by(id: params[:id]) 
+	end
+
+	def update
+		@pen = Pen.find_by(id: params[:id])
+		if @pen.update(pen_params)
+			redirect_to root_path, notice: "更新成功"
+		else
+			render :edit 
+		end
+
+	end
+
 	def destroy
 		@pen = Pen.find_by(id: params[:id])
 		@pen.destroy if @pen
