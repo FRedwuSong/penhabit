90eedb34adb90cb11a59ed568c6fce4604208c60
wusunghan   <diadora1166@gmail.com>
2018-05-28 21:08:38 +0800

    pens function CRUD

--- app/controllers/pens_controller.rb
+++ app/controllers/pens_controller.rb
@@ -3,4 +3,33 @@ before_action :authenticate_user!
 	def index
   	@pens = Pen.all	
 	end
+
+	def new
+		@pen = Pen.new
+	end	
+
+	def create
+		@pen = current_user.pens.build(pen_params)
+		if @pen.save
+			redirect_to root_path, notice: "新增成功"
+		else
+			render :new 
+		end
+	end
+
+	def destroy
+		@pen = Pen.find_by(id: params[:id])
+		@pen.destroy if @pen
+		redirect_to root_path, notice: "鋼筆資料已刪除"
+	end
+  
+  def show
+  	
+  end
+
+	private
+	def pen_params
+		params.require(:pen).permit(:name, :brand, :description, :price)
+	end
+
 end
