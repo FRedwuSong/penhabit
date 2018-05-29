e88799dc47f0fa6f33530a94d1750177522e3447
wusunghan   <diadora1166@gmail.com>
2018-05-30 01:22:41 +0800

    add User controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+	before_action :set_user , only: [:pens, :update, :edit]
+
+	def pens
+		@pens = @user.pens 
+	end
+
+	def edit
+    if current_user != @user
+      redirect_to user_path
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "資料更新成功"
+      redirect_to root_path
+    else
+      flash[:alert] = @user.errors.full_messages.to_sentence
+      render :edit
+    end
+
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users 
   resources :pens
+
+  resources :users, only: [:edit,:update] do
+    member do 
+      get :pens
+    end
+  end
+  
   root :to => "pens#index"
 end
