5f644ef06f85e3b4ec778bb399416d429efc37ef
wusunghan   <diadora1166@gmail.com>
2018-06-10 10:46:58 +0800

    solve Indentation  problem in sublime text 3

--- app/models/pen.rb
+++ app/models/pen.rb
@@ -1,12 +1,11 @@
 class Pen < ApplicationRecord
-	belongs_to :user
-	mount_uploader :image, ImageUploader
-
-	has_many :watchs, dependent: :destroy
+  belongs_to :user
+  mount_uploader :image, ImageUploader
   
+  has_many :watchs, dependent: :destroy
   has_many :watched_users, through: :watchs, source: :user
 
-	def is_watched?(user)
+  def is_watched?(user)
     watched_users.include?(user)
   end
 end
